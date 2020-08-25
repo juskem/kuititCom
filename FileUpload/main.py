1d3dcfa7561feb2d00e9d7b723474396ea0f0d0c
juskem <52775922+juskem@users.noreply.github.com>
2020-08-25 15:58:25 +0300

    Add files via upload

---
+++ FileUpload/main.py
@@ -0,0 +1,43 @@
+import os
+#import magic
+from gevent.pywsgi import WSGIServer
+import urllib.request
+from app import app
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER']="C:/Users/jussi.kemppinen/0A_REPOSITORY/Jupyter/FileUpload/images/"
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST','GET'])
+def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No file selected for uploading')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			flash('File successfully uploaded')
+			return redirect('/')
+		else:
+			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+			return redirect(request.url)
+
+if __name__ == "__main__":
+
+	app_server = WSGIServer(('192.168.1.109', 9999),app)
+	app_server.serve_forever()
